dc1b9d54573531bc13e6b02e1aca3b1a8f8652fa
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-06-01 18:43:26 +0400

    dev: add comp Tooltip

---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function InfoTooltip({ isOpen, onClose, noMistake }) {
+  return (
+    <aside className={`popup ${isOpen && "popup_opened"}`}>
+      <div className="popup__container">
+        <button
+          className="popup__close-button"
+          type="button"
+          onClick={onClose}
+        ></button>
+        <div className="popup__form">
+          <svg
+            className={`infoTooltip__pic ${
+              noMistake ? "infoTooltip__pic_success" : "infoTooltip__pic_fail"
+            }`}
+          ></svg>
+          <h2 className="infoTooltip__title">
+            {noMistake
+              ? "Вы успешно зарегистрировались!"
+              : "Что-то пошло не так! Попробуйте ещё раз."}
+          </h2>
+        </div>
+      </div>
+    </aside>
+  );
+}
+
+export default InfoTooltip;
